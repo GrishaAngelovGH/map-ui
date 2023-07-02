506371d7b22bf1e6fd9deb4035a01bf23b77ccb0
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-02 09:21:14 +0300

    Apply eslint rules to codebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <div className="row g-0">
-      <div className="col-md-12">
+    <div className='row g-0'>
+      <div className='col-md-12'>
         App
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
