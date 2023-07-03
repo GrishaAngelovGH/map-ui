3833b32dfb60557b1da8a0becf4f3d6ec3fb61c4
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-03 12:56:09 +0300

    Add margin to MapButton

--- src/components/Map/MapButton/MapButton.js
+++ src/components/Map/MapButton/MapButton.js
@@ -1,6 +1,6 @@
 const MapButton = ({ icon, title, onClick }) => (
   <div className='leaflet-right'>
-    <div className='leaflet-control leaflet-bar'>
+    <div className='leaflet-control leaflet-bar mt-2'>
       <button className='btn btn-light' onClick={onClick} title={title}>
         <i className={`bi bi-${icon}`}></i>
       </button>
