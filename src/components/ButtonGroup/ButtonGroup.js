d352303773adfd6cfe3d21a10d2f0684a52f0489
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-03 12:41:49 +0300

    Add show route button in ButtonGroup

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -1,9 +1,10 @@
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
-const ButtonGroupWrapper = ({ onShowMarkers }) => (
+const ButtonGroupWrapper = ({ onShowMarkers, onShowRoute }) => (
   <ButtonGroup>
-    <Button variant='success' onClick={onShowMarkers}>Show Markers</Button>
+    <Button variant='outline-success' className='text-white' onClick={onShowMarkers}>Show Markers</Button>
+    <Button variant='outline-success' className='text-white' onClick={onShowRoute}>Show Route</Button>
   </ButtonGroup>
 )
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import ButtonGroup from '../ButtonGroup'
 
 import './Header.scss'
 
-const Header = ({ onShowMarkers }) => (
+const Header = ({ onShowMarkers, onShowRoute }) => (
   <div className='row g-0 bg-header justify-content-center text-center p-3'>
     <div className='col-md-6'>
       <h1 className='text-white'>Map UI</h1>
-      <ButtonGroup onShowMarkers={onShowMarkers} />
+      <ButtonGroup
+        onShowMarkers={onShowMarkers}
+        onShowRoute={onShowRoute}
+      />
     </div>
   </div>
 )
