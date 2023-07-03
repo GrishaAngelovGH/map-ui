54190278860da361d095b5ac6a3ed2725103a33d
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-03 10:34:24 +0300

    Add title prop to MapButton

--- src/components/Map/MapButton/MapButton.js
+++ src/components/Map/MapButton/MapButton.js
@@ -1,7 +1,7 @@
-const MapButton = ({ icon, onClick }) => (
+const MapButton = ({ icon, title, onClick }) => (
   <div className='leaflet-right'>
     <div className='leaflet-control leaflet-bar'>
-      <button className='btn btn-light' onClick={onClick}>
+      <button className='btn btn-light' onClick={onClick} title={title}>
         <i className={`bi bi-${icon}`}></i>
       </button>
     </div>
--- src/components/Map/MapButton/MapButton.test.js
+++ src/components/Map/MapButton/MapButton.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import MapButton from './MapButton'
 
 test('should render MapButton component', () => {
-  const view = render(<MapButton icon='list' />)
+  const view = render(<MapButton icon='list' title='Show List' />)
 
   expect(view).toMatchSnapshot()
 })
