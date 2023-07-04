03a0c78407ff9009f45fe87fa553bed60bc6d7a1
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-04 13:22:50 +0300

    Remove border of MapButton

--- src/components/Map/MapButton/MapButton.js
+++ src/components/Map/MapButton/MapButton.js
@@ -1,6 +1,6 @@
 const MapButton = ({ icon, title, onClick }) => (
   <div className='leaflet-right'>
-    <div className='leaflet-control leaflet-bar mt-2'>
+    <div className='leaflet-control mt-2'>
       <button className='btn btn-light' onClick={onClick} title={title}>
         <i className={`bi bi-${icon}`}></i>
       </button>
