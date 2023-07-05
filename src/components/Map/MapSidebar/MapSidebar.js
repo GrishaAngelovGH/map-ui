02c7a69a16679ed7b59eccc4da84bfd65c6fdce9
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-05 11:23:18 +0300

    Implement MapSidebar component

--- src/components/Map/MapSidebar/MapSidebar.js
+++ src/components/Map/MapSidebar/MapSidebar.js
@@ -1,10 +1,28 @@
+import { Fragment } from 'react'
 import Sidebar from '../../Sidebar'
+import Place from './Place'
 
 const MapSidebar = ({ showSidebar, onHide }) => {
+  const places = [
+    { title: "Bill Baker's Street Restaurant", open: true },
+    { title: 'Pizza Restaurant Waterloo', open: false },
+    { title: 'Leicester Square Restaurant', open: true },
+    { title: 'Mayfair Restaurant', open: true },
+    { title: 'Chelsea Restaurant', open: true }
+  ]
+
   return (
     <Sidebar
-      title='Title'
-      body={<div>Body</div>}
+      title='Favorite Places'
+      body={
+        <Fragment>
+          {
+            places.map((v, i) => (
+              <Place key={i} title={v.title} open={v.open} />
+            ))
+          }
+        </Fragment>
+      }
       showSidebar={showSidebar}
       onHide={onHide}
     />
