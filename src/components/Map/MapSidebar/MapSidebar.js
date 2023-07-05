b0e708c9ad685b864c8c01d13e5b1181dd9580db
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-05 12:46:39 +0300

    Show PlaceModal when click on given Place in MapSidebar

--- src/components/Map/MapSidebar/MapSidebar.js
+++ src/components/Map/MapSidebar/MapSidebar.js
@@ -1,28 +1,61 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import Sidebar from '../../Sidebar'
 import Place from './Place'
+import PlaceModal from './Place/PlaceModal'
 
 const MapSidebar = ({ showSidebar, onHide }) => {
+  const [showPlace, setShowPlace] = useState(false)
+  const [placeId, setPlaceId] = useState(0)
+
+  const handlePlaceClick = id => {
+    setShowPlace(true)
+    setPlaceId(id)
+  }
+
+  const hidePlace = () => {
+    setShowPlace(false)
+  }
+
   const places = [
-    { title: "Bill Baker's Street Restaurant", open: true },
-    { title: 'Pizza Restaurant Waterloo', open: false },
-    { title: 'Leicester Square Restaurant', open: true },
-    { title: 'Mayfair Restaurant', open: true },
-    { title: 'Chelsea Restaurant', open: true }
+    { id: 1, title: "Bill Baker's Street Restaurant", open: true },
+    { id: 2, title: 'Pizza Restaurant Waterloo', open: false },
+    { id: 3, title: 'Leicester Square Restaurant', open: true },
+    { id: 4, title: 'Mayfair Restaurant', open: true },
+    { id: 5, title: 'Chelsea Restaurant', open: true }
   ]
+
+  const place = places.find(v => v.id === placeId)
+
+  const body = (
+    <Fragment>
+      {
+        places.map(v => (
+          <Place
+            key={v.id}
+            id={v.id}
+            title={v.title}
+            open={v.open}
+            onClick={handlePlaceClick}
+          />
+        ))
+      }
+
+      {
+        showPlace && (
+          <PlaceModal
+            show={showPlace}
+            place={place}
+            onHide={hidePlace}
+          />
+        )
+      }
+    </Fragment>
+  )
 
   return (
     <Sidebar
       title='Favorite Places'
-      body={
-        <Fragment>
-          {
-            places.map((v, i) => (
-              <Place key={i} title={v.title} open={v.open} />
-            ))
-          }
-        </Fragment>
-      }
+      body={body}
       showSidebar={showSidebar}
       onHide={onHide}
     />
