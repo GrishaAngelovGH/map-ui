24053f1389b49ed3ef434ecb3ca2658d5d3cd82f
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-06 09:32:31 +0300

    Redesign Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import ButtonGroup from './ButtonGroup'
 import './Footer.scss'
 
 const Footer = ({ onShowUndergroundLocations }) => (
-  <div className='row g-0 justify-content-center align-items-center bg-footer p-2'>
-    <div className='col-2 col-md-2'>
+  <div className='row g-0 justify-content-center align-items-center'>
+    <div className='col-10 col-md-4 bg-footer rounded border border-2 shadow p-2 d-flex justify-content-center'>
       <ButtonGroup onShowUndergroundLocations={onShowUndergroundLocations} />
     </div>
   </div>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -33,7 +33,7 @@ Layout.Body = ({ children }) => (
 )
 
 Layout.Footer = ({ children }) => (
-  <div className='row g-0 fixed-bottom'>
+  <div className='row g-0'>
     <div className='col-md-12'>
       {children}
     </div>
