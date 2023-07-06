e2164a42cabacd62172caf6535cdd6805cef9494
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-06 13:08:46 +0300

    Visualize all boroughs in BoroughsSidebar

--- src/components/Map/Sidebars/BoroughsSidebar/BoroughsSidebar.js
+++ src/components/Map/Sidebars/BoroughsSidebar/BoroughsSidebar.js
@@ -1,10 +1,30 @@
+import { useContext } from 'react'
+import LondonAreaContext from '../../../../contexts/LondonAreaContext'
 import Sidebar from '../../../Sidebar'
 
 const BoroughsSidebar = ({ showSidebar, onHide }) => {
+  const londonArea = useContext(LondonAreaContext)
+
+  const body = (
+    <div>
+      {
+        londonArea.features.map(v => {
+          const id = v.properties['cartodb_id']
+
+          return (
+            <div key={id}>
+              <h5>#{id.toString().padStart(2, '0')} {v.properties.name}</h5>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+
   return (
     <Sidebar
       title='London Boroughs'
-      body={<div>Body</div>}
+      body={body}
       showSidebar={showSidebar}
       onHide={onHide}
     />
--- src/components/Map/Sidebars/BoroughsSidebar/BoroughsSidebar.test.js
+++ src/components/Map/Sidebars/BoroughsSidebar/BoroughsSidebar.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import BoroughsSidebar from './BoroughsSidebar'
 
 test('should render BoroughsSidebar component', () => {
-  const view = render(<BoroughsSidebar />)
+  const view = render(<BoroughsSidebar showSidebar />)
 
   expect(view).toMatchSnapshot()
 })
