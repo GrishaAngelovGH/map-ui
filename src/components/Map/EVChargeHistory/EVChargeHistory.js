35e4925caa42f59a04a6047ff5a10f7488786c4d
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-11 10:13:15 +0300

    Integrate BatteryMeasure in EVChargeHistory

--- src/components/Map/EVChargeHistory/EVChargeHistory.js
+++ src/components/Map/EVChargeHistory/EVChargeHistory.js
@@ -1,7 +1,14 @@
 import Modal from 'react-bootstrap/Modal'
 import Summary from './Summary'
+import BatteryMeasure from './BatteryMeasure'
 
 const EVChargeHistory = ({ show, onHide }) => {
+  const history = [
+    { location: { city: 'London', country: 'England, UK' }, battery: 100 },
+    { location: { city: 'London', country: 'England, UK' }, battery: 90 },
+    { location: { city: 'London', country: 'England, UK' }, battery: 80 },
+  ]
+
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header closeButton>
@@ -10,8 +17,14 @@ const EVChargeHistory = ({ show, onHide }) => {
           <span className='mx-2'>EV Charge History</span>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className='overflow-auto' style={{ height: 500 }}>
         <Summary />
+
+        <div className='p-3'>
+          {
+            history.map((v, i) => (<BatteryMeasure key={i} measure={v} />))
+          }
+        </div>
       </Modal.Body>
     </Modal>
   )
