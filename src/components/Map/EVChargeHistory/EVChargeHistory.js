f4ba0bcc883655dd5da425536d69732b70146278
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-12 09:27:03 +0300

    Integrate ChargeDuration in EVChargeHistory

--- src/components/Map/EVChargeHistory/EVChargeHistory.js
+++ src/components/Map/EVChargeHistory/EVChargeHistory.js
@@ -1,12 +1,17 @@
 import Modal from 'react-bootstrap/Modal'
-import Summary from './Summary'
 import BatteryMeasure from './BatteryMeasure'
+import ChargeDuration from './ChargeDuration'
+import Summary from './Summary'
 
 const EVChargeHistory = ({ show, onHide }) => {
   const history = [
-    { location: { city: 'London', country: 'England, UK' }, battery: 100 },
-    { location: { city: 'London', country: 'England, UK' }, battery: 90 },
-    { location: { city: 'London', country: 'England, UK' }, battery: 80 },
+    { id: 1, location: { city: 'London', country: 'England, UK' }, battery: 100, charge: false, distanceInKm: 20, durationTimeInMin: 30 },
+    { id: 2, location: { city: 'London', country: 'England, UK' }, battery: 90, charge: false, distanceInKm: 24, durationTimeInMin: 28 },
+    { id: 3, location: { city: 'London', country: 'England, UK' }, battery: 70, charge: false, distanceInKm: 46, durationTimeInMin: 55 },
+    { id: 4, location: { city: 'London', country: 'England, UK' }, currentBatteryCharge: 85, prevBatteryCharge: 70, durationTimeInMin: 35, charge: true, name: 'ChargePoint - 123 Road', location: 'A12 B34 London, UK' },
+    { id: 5, location: { city: 'London', country: 'England, UK' }, battery: 65, charge: false, distanceInKm: 32, durationTimeInMin: 40 },
+    { id: 6, location: { city: 'London', country: 'England, UK' }, battery: 60, charge: false, distanceInKm: 5, durationTimeInMin: 10 },
+    { id: 7, location: { city: 'London', country: 'England, UK' }, currentBatteryCharge: 95, prevBatteryCharge: 60, durationTimeInMin: 75, charge: true, name: 'ChargePoint - 123 Road', location: 'A12 B34 London, UK' }
   ]
 
   return (
@@ -22,7 +27,11 @@ const EVChargeHistory = ({ show, onHide }) => {
 
         <div className='p-3'>
           {
-            history.map((v, i) => (<BatteryMeasure key={i} measure={v} />))
+            history.map(v => (
+              v.charge ?
+                <ChargeDuration key={v.id} duration={v} /> :
+                <BatteryMeasure key={v.id} measure={v} />
+            ))
           }
         </div>
       </Modal.Body>
