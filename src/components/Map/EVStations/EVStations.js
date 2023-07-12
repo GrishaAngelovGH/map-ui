52337d26f6e1dc1eda5d3b1a5b0bf274aa8ce452
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-12 11:27:56 +0300

    Implement EVStations component

--- src/components/Map/EVStations/EVStations.js
+++ src/components/Map/EVStations/EVStations.js
@@ -1,16 +1,43 @@
+import Carousel from 'react-bootstrap/Carousel'
 import Modal from 'react-bootstrap/Modal'
+import EVStation from './EVStation/EVStation'
 
-const EVStations = ({ show, onHide }) => (
-  <Modal show={show} onHide={onHide} backdrop={false}>
-    <Modal.Header closeButton>
-      <Modal.Title>
-        <i className='bi bi-ev-station'></i>
-        <span className='mx-2'>EV Stations Near You</span>
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-    </Modal.Body>
-  </Modal>
-)
+import './EVStations.scss'
+
+const EVStations = ({ show, onHide }) => {
+  const evstations = [
+    { id: 1, name: 'London EV Charging Station #1', location: 'A12 B34 London, UK', connector: 'Type 2 or CCS 2', distance: 1.8, time: 8 },
+    { id: 2, name: 'London EV Charging Station #2', location: 'A12 B34 London, UK', connector: 'Type 2 or CCS 2', distance: 2.5, time: 15 },
+    { id: 3, name: 'London EV Charging Station #3', location: 'A12 B34 London, UK', connector: 'Type 2 or CCS 2', distance: 5.4, time: 20 }
+  ]
+
+  return (
+    <Modal show={show} onHide={onHide} backdrop={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <i className='bi bi-ev-station'></i>
+          <span className='mx-2'>EV Stations Near You</span>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='bg-secondary bg-opacity-75 d-flex justify-content-center align-items-center' style={{ height: 350 }}>
+        <Carousel>
+          {
+            evstations.map(v => (
+              <Carousel.Item key={v.id}>
+                <EVStation
+                  name={v.name}
+                  location={v.location}
+                  connector={v.connector}
+                  distance={v.distance}
+                  time={v.time}
+                />
+              </Carousel.Item>
+            ))
+          }
+        </Carousel>
+      </Modal.Body>
+    </Modal>
+  )
+}
 
 export default EVStations
