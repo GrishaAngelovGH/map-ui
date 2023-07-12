3b29aadfa8a5b1365fe7b719b07822952fe79c10
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-12 10:28:15 +0300

    Add charging stations button in ButtonGroup

--- src/components/Map/MapMenu/ButtonGroup/ButtonGroup.js
+++ src/components/Map/MapMenu/ButtonGroup/ButtonGroup.js
@@ -3,14 +3,18 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import ldnUndergroundIcon from './ldn-underground.png'
 import mapIcon from './map.png'
+import chargingStationIcon from './charging-station.png'
 
-const ButtonGroupWrapper = ({ onShowUndergroundLocations, onShowBoroughs }) => (
+const ButtonGroupWrapper = ({ onShowUndergroundLocations, onShowBoroughs, onShowEVStations }) => (
   <ButtonGroup>
     <Button variant='outline-success' title='Underground Locations' onClick={onShowUndergroundLocations}>
       <Image src={ldnUndergroundIcon} width={40} />
     </Button>
     <Button variant='outline-success' title='London Boroughs' onClick={onShowBoroughs}>
       <Image src={mapIcon} width={40} />
+    </Button>
+    <Button variant='outline-success' title='EV Stations' onClick={onShowEVStations}>
+      <Image src={chargingStationIcon} width={40} />
     </Button>
   </ButtonGroup>
 )
