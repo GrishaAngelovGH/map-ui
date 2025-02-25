641da0348c0ba0d8b631e025bab9f443c79fdaf6
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-25 14:09:05 +0200

    Integrate @vitest/eslint-plugin into ESLint configuration for Vitest support

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import vitest from '@vitest/eslint-plugin'
 
 export default [
   { ignores: ['dist'] },
@@ -10,7 +11,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        'L': 'readonly'
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -22,6 +26,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      vitest
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +38,20 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off',
+      'react/display-name': 'off'
     },
   },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly'
+      }
+    }
+  }
 ]
--- src/components/Map/MapillaryViewer/MapillaryViewer.jsx
+++ src/components/Map/MapillaryViewer/MapillaryViewer.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useRef } from 'react'
-
+import { useRef, useEffect } from 'react'
 import { Viewer } from 'mapillary-js'
 
 const accessToken = import.meta.env.VITE_REACT_APP_DEMO_MAPILLARY_API_ACCESS_TOKEN
 
 const MapillaryViewer = ({ fullscreen }) => {
   const containerRef = useRef()
-  let viewer
+  const viewerRef = useRef()
 
   const height = {
     true: 'calc(100vh - 100px)',
@@ -14,18 +13,18 @@ const MapillaryViewer = ({ fullscreen }) => {
   }
 
   useEffect(() => {
-    viewer = new Viewer({
+    viewerRef.current = new Viewer({
       accessToken,
       container: containerRef.current,
       imageId: '2891821574414732',
     })
 
     return () => {
-      if (viewer) {
-        viewer.remove()
+      if (viewerRef.current) {
+        viewerRef.current.remove()
       }
     }
-  }, [containerRef.current])
+  }, [])
 
   return (
     <div ref={containerRef} style={{ width: '100%', height: height[fullscreen] }} />
